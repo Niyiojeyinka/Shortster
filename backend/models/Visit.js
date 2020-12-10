f9539b54956f97e8b2c32bd80259f38d38ac2060
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-12-10 01:29:18 +0100

    feature:(Backend) remved plaform,browser column

--- backend/models/Visit.js
+++ backend/models/Visit.js
@@ -2,12 +2,7 @@ module.exports = (sequelize, Sequelize) => {
   const Visit = sequelize.define("Visit", {
     shortCode: {
       type: Sequelize.STRING,
-    },
-    platform: {
-      type: Sequelize.STRING,
-    },
-    browser: {
-      type: Sequelize.STRING,
+      allowNull: false,
     },
   });
 
